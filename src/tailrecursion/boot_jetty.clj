35618464ab73217fd7b1f9e913f8feb3d0079a88
Micha Niskin <micha.niskin@gmail.com>
2015-12-06 16:33:41 -0500

    Revert "Fix windows issue where open files interfere with fileset operations"
    This reverts commit 346bbf96b428ebdea22b84bb8f98220335ac4775.

--- src/tailrecursion/boot_jetty.clj
+++ src/tailrecursion/boot_jetty.clj
@@ -1,31 +1,25 @@
 (ns tailrecursion.boot-jetty
   {:boot/export-tasks true}
   (:require
-    [boot.pod  :as pod]
-    [boot.file :as file]
-    [boot.core :as boot]
-    [boot.util :as util]))
+   [boot.pod  :as pod]
+   [boot.core :as boot]
+   [boot.util :as util] ))
 
 (def ^:private deps
   (delay (remove pod/dependency-loaded?
-                 '[[org.eclipse.jetty/jetty-servlet "9.3.1.v20150714"]
-                   [org.eclipse.jetty/jetty-server  "9.3.1.v20150714"]
-                   [org.eclipse.jetty/jetty-webapp  "9.3.1.v20150714"] ])))
+   '[[org.eclipse.jetty/jetty-servlet "9.3.1.v20150714"]
+     [org.eclipse.jetty/jetty-server  "9.3.1.v20150714"]
+     [org.eclipse.jetty/jetty-webapp  "9.3.1.v20150714"] ])))
 
 (boot/deftask serve
   "Serve the application, reloading all namspaces with each subsequent invocation."
   [p port PORT int "The port the server will bind to."]
-  (let [pod-env (-> (boot/get-env)
-                    (dissoc :source-paths)
-                    (update :dependencies into (vec (seq @deps))))
+  (let [pod-env (-> (boot/get-env) (dissoc :source-paths) (update :dependencies into (vec (seq @deps))))
         pod     (future (pod/make-pod pod-env))
         webapp  (boot/tmp-dir!)
-        serve   (delay
-                  (pod/with-call-in @pod
-                    (tailrecursion.boot-jetty.impl/serve ~(.getPath webapp) ~port)))]
+        serve   (delay (pod/with-call-in @pod (tailrecursion.boot-jetty.impl/serve ~(.getPath webapp) ~port))) ]
     (util/info "Starting Jetty on port %s...\n" port)
     (boot/with-pre-wrap fileset
-      (binding [boot.file/*hard-link* false]
-        (apply boot/sync! webapp (boot/output-dirs fileset)))
+      (apply boot/sync! webapp (boot/output-dirs fileset))
       @serve
-      fileset)))
+     fileset )))
